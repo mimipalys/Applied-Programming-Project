f180d2c1a6d2b1b1911809a7131a532ebf8bdc3d
noomywastaken <neuman.alkhalil@outlook.com>
2022-04-19 12:32:36 +0200

    React Navigation
    Simple navigation with parameter passing to previous screen

--- busily/App.js
+++ busily/App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, TextInput, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function HomeScreen({ navigation, route }) {
+    React.useEffect(() => {
+        if (route.params?.post) {
+            // Update business card in db
+        }
+    }, [route.params?.post]);
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Button
+                title="Create post"
+                onPress={() => navigation.navigate('CreatePost')}
+            />
+            <Text style={{ margin: 10 }}>Business Card: {route.params?.post}</Text>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function CreatePostScreen({ navigation, route }) {
+    const [postText, setPostText] = React.useState('');
+
+    return (
+        <>
+            <TextInput
+                multiline
+                placeholder="Full name, Occupation"
+                style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+                value={postText}
+                onChangeText={setPostText}
+            />
+            <Button
+                title="Done"
+                onPress={() => {
+                    // Pass and merge params back to home screen
+                    navigation.navigate({
+                        name: 'Home',
+                        params: { post: postText },
+                        merge: true,
+                    });
+                }}
+            />
+        </>
+    );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator mode="modal">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="CreatePost" component={CreatePostScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
