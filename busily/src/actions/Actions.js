54a365dc4f70155682078b3973c4b1c7f7757667
TheRealDemmy <Dendimir101@yahoo.com>
2022-06-18 17:37:19 +0200

    Pushing stuff - Demmy

--- busily/src/actions/Actions.js
+++ busily/src/actions/Actions.js
@@ -19,7 +19,7 @@ export const fetchDataAll = () => (dispatch) => {
 };
 export const fetchMainUser = () => (dispatch) => {
     axios.get("https://jsonplaceholder.typicode.com/users/1").then((response) => {
-        const list = [response.data]
+        const list = [response.data];
         dispatch(dataFetchOne(list));
     });
 };
--- busily/src/reducers/CombineReducers.js
+++ busily/src/reducers/CombineReducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { app } from '../reducers/Reducer';
+import { app } from './Reducer';
 
 const rootReducer = combineReducers({
     app
