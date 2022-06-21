7a80d2363430ae0b78f4144d1d37b17d89c86861
noomywastaken <neuman.alkhalil@outlook.com>
2022-06-21 04:12:27 +0200

    Up to Lambda
    18:00 lambda, cannot access try again next session

---
+++ busily/src/aws-exports.js
@@ -0,0 +1,12 @@
+const config = {
+    region: 'eu-central-1',
+    "aws_cloud_logic_custom": [
+        {
+            "name": "arcapi",
+            "endpoint": "",
+            "region": "eu-central-1"
+        }
+    ]
+}
+
+export default config
